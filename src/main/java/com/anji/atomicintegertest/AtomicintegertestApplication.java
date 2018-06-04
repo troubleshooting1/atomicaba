56799a457fe157dabe8f324764db1d6c42028a73
troubleshooting1 <467503768@qq.com>
2018-06-04 17:16:42 +0800

    initial commit

---
+++ src/main/java/com/anji/atomicintegertest/AtomicintegertestApplication.java
@@ -0,0 +1,12 @@
+package com.anji.atomicintegertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicintegertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicintegertestApplication.class, args);
+	}
+}
